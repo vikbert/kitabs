3c3728fd5f370a464799cdd3f7c18f933176e6b5
Xun Zhou <segentor@gmail.com>
2020-05-11 09:01:20 +0200

    updates

--- src/pages/Newtab/alert/Alert.jsx
+++ src/pages/Newtab/alert/Alert.jsx
@@ -5,7 +5,7 @@ import TimerDisplay from "./TimerDisplay";
 
 const OPTIONS = [5, 10, 15, 25];
 const Alert = () => {
-    const [duration, setDuration] = useState(3);
+    const [duration, setDuration] = useState(5);
     const {visible, show} = useVisible();
 
     const handleOnClick = (event, value) => {
@@ -16,13 +16,13 @@ const Alert = () => {
     return (
         <>
             <Popup show={visible}>
-                <TimerDisplay counterInSeconds={duration}/>
+                <TimerDisplay counterInSeconds={duration * 60}/>
             </Popup>
             {OPTIONS.map((option) => (
                 <div key={option} className={'button-alert'}
                      onClick={(event, value) => handleOnClick(event, option)}>
                     <span className="icon icon-alarm"/>
-                    <span>{`${option}m`}</span>
+                    <span>{`${option}"`}</span>
                 </div>
             ))}
         </>
