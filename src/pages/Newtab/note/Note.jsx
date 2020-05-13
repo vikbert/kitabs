67f129ec395020b37f77bc391867f218d59087a7
Xun Zhou <segentor@gmail.com>
2020-05-14 00:38:47 +0200

    updates

--- src/pages/Newtab/note/Note.jsx
+++ src/pages/Newtab/note/Note.jsx
@@ -1,40 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Article } from 'react-weui';
+import React, {useEffect, useState} from 'react';
+import {Article} from 'react-weui';
 import noteStore from '../../../storage/NoteStore';
 
 const Note = () => {
-  const [notes, setNotes] = useState({});
+    const [notes, setNotes] = useState({});
+    const [editKey, setEditKey] = useState(null);
 
-  const handleDeleteNote = (noteId) => {
-    const cloned = { ...notes };
-    delete cloned[noteId];
-    setNotes(cloned);
+    const handleDeleteNote = (noteId) => {
+        const cloned = {...notes};
+        delete cloned[noteId];
+        setNotes(cloned);
 
-    noteStore.delete(noteId);
-  };
-  useEffect(() => {
-    setNotes(noteStore.loadAll());
-  }, []);
+        noteStore.delete(noteId);
+    };
 
-  return (
-    <Article className={'fade-in'}>
-      <ul>
-        {Object.keys(notes).map((key) => (
-          <li key={key}>
-            <a href="#">
-              <span
-                className="close-icon"
-                onClick={() => handleDeleteNote(key)}
-              >
-                {'X'}
-              </span>
-              <span>{notes[key].content}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </Article>
-  );
+    const handleClickNote = (noteId) => {
+        setEditKey(noteId);
+    };
+
+    useEffect(() => {
+        setNotes(noteStore.loadAll());
+    }, []);
+
+    return (
+        <Article className={'fade-in'}>
+            <ul>
+                {Object.keys(notes).map((key) => (
+                    <li key={key}>
+                        <a href="#" className={editKey === key ? 'edit': null} onClick={() => handleClickNote(key)}>
+                          <span className="close-icon"
+                                onClick={() => handleDeleteNote(key)}>
+                          {'X'}
+                          </span>
+                            <span>{notes[key].content}</span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </Article>
+    );
 };
 
 export default Note;
