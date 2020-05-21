61b30e33037bd10de7c701db8f12e1d82c35b278
Xun Zhou <segentor@gmail.com>
2020-05-21 06:59:14 +0200

    updates

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,4 @@
 import '../../assets/img/icon-34.png';
-import '../../assets/img/icon-128.png';
 import BookmarkStore from "../../storages/BookmarkStore";
 import {logBookmarks, logPopup} from "../../helpers/Logger";
 
